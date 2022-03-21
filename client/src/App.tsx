28206bf2b885621f76a8f34f9e4218c148df70f6
saifulshihab <shihabmd1970@gmail.com>
2022-03-21 23:16:05 +0600

    feat: install tailwindcss

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App: React.FC = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className="text-7xl font-extrabold text-blue-500">
+      Hello From Tailwind CSS
+    </div>
+  );
 };
 
 export default App;
