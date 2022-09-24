39fd8aae9eee109be7ecfca68e125167617eed62
saifulshihab <shihabmd1970@gmail.com>
2022-09-24 14:47:15 +0600

    refactor: more deletion

--- packages/server/src/database/index.ts
+++
@@ -1,8 +0,0 @@
-import { ICard } from "@card-32/common/types/card";
-import { IPlayer } from "@card-32/common/types/player";
-
-export const players: IPlayer[] = [];
-export const roomPlayers = (roomId: string) =>
-  players.filter((player) => player.playerId === roomId);
-
-export const cards: ICard[] = [];
