79097c6190dbebe430277aa38319d5c219fe3f5e
saifulshihab <shihabmd1970@gmail.com>
2022-10-28 19:05:06 +0600

    feat: random emoji for global chat

--- packages/web/src/components/organisms/chat/ChatMessage.tsx
+++ packages/web/src/components/organisms/chat/ChatMessage.tsx
@@ -2,6 +2,45 @@ import { TGenericMessage } from "@card-32/common/types/player";
 import React, { useMemo } from "react";
 import { colors } from "../../../utils/color";
 
+// some random emoji for global chat username
+export const emoji: string[] = [
+  "🤡",
+  "🤪",
+  "💩",
+  "😈",
+  "👻",
+  "💀",
+  "👽",
+  "🦸",
+  "🥷",
+  "🫅",
+  "🧚‍♀️",
+  "🧜‍♀️",
+  "🧞",
+  "🧚‍♂️",
+  "🦁",
+  "🐝",
+  "🦉",
+  "🐔",
+  "🐼",
+  "🌳",
+  "🦜",
+  "⛄️",
+  "🍀",
+  "🐿",
+  "🐇",
+  "🐕",
+  "🍎",
+  "🍇",
+  "🍉",
+  "🥑",
+  "🍒",
+  "🍗",
+  "🫐",
+  "🍭",
+  "🥣",
+];
+
 interface IProps {
   message: TGenericMessage;
 }
@@ -9,32 +48,33 @@ interface IProps {
 const ChatMessage: React.FC<IProps> = (props) => {
   const { message } = props;
   const colorIndex = useMemo(() => Math.floor(Math.random() * 6), []);
+  const emojiIndex = useMemo(() => Math.floor(Math.random() * 34), []);
   return (
     <div className="flex gap-1">
       {message.username ? (
-        <>
-          <p>
-            <span
-              style={{
-                color: `${colors[colorIndex]}`,
-              }}
-              className="font-semibold"
-            >
-              {message.username}
-            </span>
-          </p>
-          <span className="font-bold">:</span>
-        </>
-      ) : (
-        <span
-          className="font-bold"
+        <p
           style={{
             color: `${colors[colorIndex]}`,
           }}
+          className="font-semibold"
         >
-          :
+          {message.username}
+        </p>
+      ) : (
+        <span role="img" aria-label="emoji">
+          {emoji[emojiIndex]}
         </span>
       )}
+
+      <span
+        className="font-bold"
+        style={{
+          color: `${colors[colorIndex]}`,
+        }}
+      >
+        :
+      </span>
+
       <p>{message.message}</p>
     </div>
   );
