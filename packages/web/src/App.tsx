7074a80933372661b8a53170167572c06ce1c998
saifulshihab <shihabmd1970@gmail.com>
2024-06-30 18:34:25 +0300

    chore: move css files

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -3,14 +3,14 @@ import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { TouchBackend } from "react-dnd-touch-backend";
 import { Toaster } from "react-hot-toast";
-import "./App.css";
+import "./assets/styles/App.css";
 import { AuthProvider } from "./contexts/AuthProvider";
 import { CardsProvider } from "./contexts/CardsProvider";
 import { RoomProvider } from "./contexts/RoomProvider";
 import { SocketProvider } from "./contexts/SocketProvider";
 import { ThemeProvider } from "./contexts/ThemeProvider";
-import Router from "./routes/Router";
 import { useWindow } from "./hooks/useWindow";
+import Router from "./routes/Router";
 
 const App: React.FC = () => {
   const { width } = useWindow();
